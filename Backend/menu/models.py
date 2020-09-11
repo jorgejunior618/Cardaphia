3baf9c4692a85462f08a339876f0939065596287
jorgejunior618 <jorgejunior618@gmail.com>
2020-09-11 20:20:12 -0300

    integrando a listagem de pratos(menu) com o frontend

--- Backend/menu/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Menu (models.Model):
-    
-    def __str__ (self):
-        return f'Menu {self.id}'
--- Frontend/src/Classes/Table.js
+++
@@ -1,8 +0,0 @@
-class Table {
-  constructor (code) {
-    this._code = code;
-    this._isOcupied = false;
-  }
-}
-
-export default Table;
